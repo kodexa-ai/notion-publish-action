7401815e52c83bbf9853b039d34c756d3532772f
Philip Dodds <philip.dodds@me.com>
2022-04-13 07:37:47 -0400

    Fix param

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,15 @@ import {Client} from '@notionhq/client'
 
 async function run(): Promise<void> {
   try {
-
     // Initializing a client
     const notion = new Client({
       auth: core.getInput('notion_token')
     })
 
     await notion.pages.create({
-      parent: {database_id: core.getInput('notion_database_id')},
+      parent: {database_id: core.getInput('database_id')},
       properties: JSON.parse(core.getInput('payload'))
     })
-
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
